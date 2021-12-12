cbcf5c3fda739cc2ea9bfc7e79bdf06fd1ca1256
Diyar Turan <5130@htl.rennweg.at>
2021-12-12 22:03:34 +0100

    day1 part2 update

--- 2021/Day1/day1_2.java
+++ 2021/Day1/day1_2.java
@@ -3,23 +3,16 @@ package Day1;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
-import java.util.ArrayList;
 import java.util.List;
 
 public class day1_2 {
     public static void main(String[] args) throws IOException {
         List<String> lines = Files.readAllLines(Paths.get("2021\\Day1\\input.txt"));
-        List<Integer> sums = new ArrayList<>();
         int prevInt = 0;
         int counter = 0;
-        for (int i = 0; i <lines.size()-2; i++) {
-            int sum  = Integer.parseInt(lines.get(i)) + Integer.parseInt(lines.get(i+1)) +
-                    Integer.parseInt(lines.get(i+2));
-
-            sums.add(sum);
-            if (prevInt < sum) counter++;
-            prevInt = sum;
+        for (int i = 0; i <lines.size()-3; i++) {
+            if (Integer.parseInt(lines.get(i)) < Integer.parseInt(lines.get(i+3))) counter++;
         }
-        System.out.println(counter-1);
+        System.out.println(counter);
     }
 }
